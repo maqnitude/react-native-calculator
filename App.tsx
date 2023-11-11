2199cc7c7588042b33e7246618ac32dbce2fedf1
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-11-11 14:47:32 +0700

    Create the initial layout of the calculator

--- App.tsx
+++ App.tsx
@@ -1,118 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// TODO: Bring this to a separate folder (i.e. ./conponents/CustomButton.ts)
+type CustomButtonProps = {
+  onPress: () => void,
+  title: string,
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const CustomButton = ({ onPress, title }: CustomButtonProps) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const CalculatorApp = () => {
+  const onButtonPressed = () => {
+    // do nothing for now
+    // this could a switch case for different button types
+  };
+  
+  return (
+    <View style={styles.container}>
+      <View style={styles.displayContainer}>
+        <Text>The calculations will be displayed here.</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonRowContainer}>
+          {['C', '', '%', '/'].map((value, index) => (
+            <CustomButton key={index} title={value} onPress={onButtonPressed} />
+          ))} 
+        </View>
+        <View style={styles.buttonRowContainer}>
+          {['7', '8', '9', '*'].map((value, index) => (
+            <CustomButton key={index} title={value} onPress={onButtonPressed} />
+          ))} 
+        </View>
+        <View style={styles.buttonRowContainer}>
+          {['4', '5', '6', '-'].map((value, index) => (
+            <CustomButton key={index} title={value} onPress={onButtonPressed} />
+          ))} 
+        </View>
+        <View style={styles.buttonRowContainer}>
+          {['1', '2', '3', '+'].map((value, index) => (
+            <CustomButton key={index} title={value} onPress={onButtonPressed} />
+          ))}
+        </View>
+        <View style={styles.buttonRowContainer}>
+          {['', '0', '.', '='].map((value, index) => (
+            <CustomButton key={index} title={value} onPress={onButtonPressed} />
+          ))}
+        </View>
+      </View>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
   },
-  sectionTitle: {
+
+  displayContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  buttonContainer: {
+    flex: 4,
+    backgroundColor: 'gray',
+  },
+  buttonRowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: '2.5%',
+  },
+  button: {
+    minWidth: '20%',
+    minHeight: '40%',
+    padding: '5%',
+    backgroundColor: 'white',
+  },
+  buttonText: {
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    textAlign: 'center',
   },
 });
 
-export default App;
+export default CalculatorApp;
