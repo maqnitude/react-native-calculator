cdf9e52940e0577c53319db12b6f9e1658078290
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-11-12 09:32:31 +0700

    Perform pending operation before new operation. This pull pull request also adds subtraction, multiplication and division.

--- src/utils/calculator.tsx
+++ src/utils/calculator.tsx
@@ -2,7 +2,16 @@ export const performOperation = (num1: number, num2: number, operation: string):
   switch (operation) {
     case '+':
       return num1 + num2;
-    // Add other operations below
+
+    case '-':
+      return num1 - num2;
+
+    case '*':
+      return num1 * num2;
+
+    case '/':
+      return num1 / num2;
+
     default:
       return null;
   }
@@ -11,13 +20,40 @@ export const performOperation = (num1: number, num2: number, operation: string):
 export const onButtonPressed = (text: string, currentInput: string, previousInput: string | null, operation: string | null) => {
   switch (text) {
     case '+':
-      return { operation: '+', previousInput: currentInput, currentInput: "" };
+      if (operation && previousInput) {
+        const result = performOperation(Number(previousInput), Number(currentInput), operation);
+        return { operation: '+', previousInput: String(result), currentInput: '' }
+      }
+      return { operation: '+', previousInput: currentInput, currentInput: '' };
+
+    case '-':
+      if (operation && previousInput) {
+        const result = performOperation(Number(previousInput), Number(currentInput), operation);
+        return { operation: '-', previousInput: String(result), currentInput: '' }
+      }
+      return { operation: '-', previousInput: currentInput, currentInput: '' };
+
+    case '*':
+      if (operation && previousInput) {
+        const result = performOperation(Number(previousInput), Number(currentInput), operation);
+        return { operation: '*', previousInput: String(result), currentInput: '' }
+      }
+      return { operation: '*', previousInput: currentInput, currentInput: '' };
+
+    case '/':
+      if (operation && previousInput) {
+        const result = performOperation(Number(previousInput), Number(currentInput), operation);
+        return { operation: '/', previousInput: String(result), currentInput: '' }
+      }
+      return { operation: '/', previousInput: currentInput, currentInput: '' };
+
     case '=':
-      if (operation && previousInput !== null) {
+      if (operation && previousInput) {
         const result = performOperation(Number(previousInput), Number(currentInput), operation);
         return { operation: null, previousInput: null, currentInput: String(result) };
       }
       return { operation, previousInput, currentInput };
+
     default:
       return { operation, previousInput, currentInput: currentInput + text };
   }
